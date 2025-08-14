1fcd59dea5333d8094568f81136c9b0fc2c9268a
David Merino Recalde <davidmrvg@gmail.com>
2025-08-14 14:04:41 +0200

    reorganización css

--- js/site.js
+++ js/site.js
@@ -51,11 +51,9 @@ $(document).ready(function () {
 
 
 
-/* Preloader and animations */
+/* WOW Elements and Parallax Effects */
 $(window).load(function () { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow-y': 'visible'});
+	$('body').css({'overflow-y': 'visible'});
 
 	/* WOW Elements */
 	if (typeof WOW == 'function') {
